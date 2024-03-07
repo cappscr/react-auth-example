4b4237bc0cb30b9d0dadee968c3aa0317d3bb1f4
cappscr <capps.christopher@gmail.com>
2024-03-07 09:19:16 -0500

    Home built auth solution including Google Oauth

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,12 +1,37 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserInfoPage } from './pages/UserInfoPage';
+import { LogInPage } from './pages/LoginPage';
+import { SignUpPage } from './pages/SignUpPage';
+import { PrivateRoute } from './auth/PrivateRoute';
+import { PleaseVerifyEmailPage } from './pages/PleaseVerifyEmailPage';
+import { EmailVerificationLandingPage } from './pages/EmailVerificationLandingPage';
+import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
+import { PasswordResetLandingPage } from './pages/PasswordResetLandingPage';
 
 export const Routes = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/" exact>
+                <PrivateRoute path="/" exact>
                     <UserInfoPage />
+                </PrivateRoute>
+                <Route path="/verify-email/:verificationString">
+                    <EmailVerificationLandingPage />
+                </Route>
+                <Route path="/forgot-password">
+                    <ForgotPasswordPage />
+                </Route>
+                <Route path="/login">
+                    <LogInPage />
+                </Route>
+                <Route path="/reset-password/:passwordResetCode">
+                    <PasswordResetLandingPage />
+                </Route>
+                <Route path="/please-verify">
+                    <PleaseVerifyEmailPage />
+                </Route>
+                <Route path="/signup">
+                    <SignUpPage />
                 </Route>
             </Switch>
         </Router>
