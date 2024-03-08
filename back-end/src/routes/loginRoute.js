e232d623de1df5e308eac4615e8d5f428bb72ff9
cappscr <capps.christopher@gmail.com>
2024-03-08 09:33:14 -0500

    Error handling clean up

--- back-end/src/routes/loginRoute.js
+++ back-end/src/routes/loginRoute.js
@@ -21,7 +21,8 @@ export const logInRoute = {
     if (isCorrect) {
       jwt.sign({ id, isVerified, email, info }, process.env.JWT_SECRET, { expiresIn: '2d'}, (err, token) => {
         if (err) {
-          res.status(500).send(err);
+          console.log(err);
+          res.sendStatus(500);
         }
 
         res.status(200).json({ token });
