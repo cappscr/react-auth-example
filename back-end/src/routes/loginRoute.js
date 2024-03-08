db959d0f5be39e3f8c60967ab296c885bb5b8141
cappscr <capps.christopher@gmail.com>
2024-03-08 05:57:42 -0500

    Login Route updates

--- back-end/src/routes/loginRoute.js
+++ back-end/src/routes/loginRoute.js
@@ -1,5 +1,11 @@
-import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import {
+  AuthenticationDetails,
+  CognitoUserPool,
+  CognitoUserAttribute,
+  CognitoUser,
+} from 'amazon-cognito-identity-js';
+import { awsUserPool } from '../util/awsUserPool';
 import { getDbConnection } from '../db';
 
 export const logInRoute = {
@@ -8,25 +14,24 @@ export const logInRoute = {
   handler: async (req, res) => {
     const { email, password } = req.body;
 
-    const db = getDbConnection('react-auth-db');
-    const user = await db.collection('users').findOne({ email });
-
-    if (!user) return res.sendStatus(401);
-
-    const { _id: id, isVerified, passwordHash, info } = user;
-
-    const isCorrect = await bcrypt.compare(password, passwordHash);
-
-    if (isCorrect) {
-      jwt.sign({ id, isVerified, email, info }, process.env.JWT_SECRET, { expiresIn: '2d'}, (err, token) => {
-        if (err) {
-          res.status(500).send(err);
-        }
-
-        res.status(200).json({ token });
-      },);
-    } else {
-      res.sendStatus(401);
-    }
-  },
+    new CognitoUser({ Username: email, Pool: awsUserPool })
+      .authenticateUser(new AuthenticationDetails({ Username: email, Password: password }), {
+        onSuccess: async result => {
+          const db = getDbConnection('react-auth-db');
+          const user = await db.collection('users').findOne({ email });
+          const { _id: id, isVerified, info } = user;
+    
+          jwt.sign({ id, isVerified, email, info }, process.env.JWT_SECRET, { expiresIn: '2d'}, (err, token) => {
+            if (err) {
+              res.status(500);
+            }
+      
+            res.status(200).json({ token });
+          });
+        },
+        onFailure: err => {
+          res.sendStatus(401);
+        },
+      });
+  }
 }
--- back-end/src/routes/verifyEmailRoute.js
+++ back-end/src/routes/verifyEmailRoute.js
@@ -1,29 +1,32 @@
-import { ObjectID } from 'mongodb';
 import jwt from 'jsonwebtoken';
+import { CognitoUser } from 'amazon-cognito-identity-js'
 import { getDbConnection } from '../db';
+import { awsUserPool } from '../util/awsUserPool';
 
 export const verifyEmailRoute = {
   path: '/api/verify-email',
   method: 'put',
   handler: async (req, res) => {
-    const { verificationString } = req.body;
-    const db = getDbConnection('react-auth-db');
+    const { email, verificationString } = req.body;
 
-    const result = await db.collection('users').findOne({
-      verificationString,
-    });
+    new CognitoUser({ Username: email, Pool: awsUserPool })
+      .confirmRegistration(verificationString, true, async (err) => {
+        if (err) return res.status(401).json({ message: 'The email verification code is incorrect' });
 
-    if (!result) return res.status(401).json({ message: 'The email verification code is incorrect' });
+        const db = getDbConnection('react-auth-db');
 
-    const { _id: id, email, info } = result;
+        const result = await db.collection('users').findOneAndUpdate({ email }, {
+          $set: { isVerified: true }
+        }, {
+          returnOriginal: false,
+        });
 
-    await db.collection('users').updateOne({ _id: ObjectID(id) }, {
-      $set: { isVerified: true }
-    });
+        const { _id: id, info } = result.value;
 
-    jwt.sign({ id, email, isVerified: true, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
-      if (err) return res.sendStatus(500);
-      res.status(200).json({ token });
-    });
+        jwt.sign({ id, email, isVerified: true, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+          if (err) return res.sendStatus(500);
+          res.status(200).json({ token });
+        });
+      });
   }
 }
